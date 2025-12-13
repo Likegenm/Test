8b5f8fefa45c09231d24e17b16c2965b58ac7ed6
Likegenm <Oleg.25478@mail.ru>
2025-12-13 17:36:07 +0300

    Update Irina.lua

--- Irina.lua
+++ Irina.lua
@@ -1,3 +1,5 @@
+game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer("Likegenm scripts tsb script", "All")
+
 local StarterGui = game:GetService("StarterGui")
 StarterGui:SetCore("SendNotification", {
     Title = "Script Loading!",
